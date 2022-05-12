e7052095b4e78d0460ef3fa5a7dd7540c37118e6
Bob D <txcavscout@live.com>
2022-05-11 23:12:56 -0700

    Add project files.

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,23 @@
+﻿string firstName;
+
+do
+{
+    Console.Write("Enter your name. Type exit to stop: ");
+    firstName = Console.ReadLine();
+    if (firstName.ToLower() == "tim")
+    {
+        Console.WriteLine($"Welcome Professor {firstName}");
+    }
+    else if (firstName.ToLower() == "exit")
+    {
+        Console.WriteLine("...closing");
+    }
+    else
+    {
+        Console.WriteLine($"Welcome student {firstName}");
+    }
+} while (firstName != "exit");
+
+Console.WriteLine("\nGoodbye.");
+
+
